ee505e7b2d59717b00e70e8e77aca0da2316152b
Femi-ige Muyiwa <84715593+muyiwexy@users.noreply.github.com>
2022-10-17 16:26:03 +0100

    Delete lib/constants directory

--- lib/constants/app_constantants.dart
+++
@@ -1,7 +0,0 @@
-class Appconstants {
-  static const String projectid = "imageuploader";
-  static const String endpoint = "http://192.168.0.113/v1";
-  static const String dbID = "urlstore";
-  static const String collectionID = "mainstorage";
-  static const String bucketId = "image_upload";
-}
